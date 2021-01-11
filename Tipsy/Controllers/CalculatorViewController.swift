6687abc807e7549c66db4dc10d687d0f0e2fb623
Aleksandr802 <savs802@gmail.com>
2021-01-11 16:19:30 +0200

    initial commit

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
    
-    var tip = 0.10
-    var numberOfPeople = 0
-    var billTotal = 0.0
-    var finalResult = "0.0"
-    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    var tip = 0.10
+    var numberOfPeople = 0
+    var billTotal = 0.0
+    var finalResult = "0.0"
     
     @IBAction func tipChanged(_ sender: UIButton) {
         
@@ -28,13 +28,11 @@ class CalculatorViewController: UIViewController {
         zeroPctButton.isSelected = false
         tenPctButton.isSelected = false
         twentyPctButton.isSelected = false
-        
         sender.isSelected = true
         
         let buttonTitle = sender.currentTitle!
         let buttonTitleMinusPercentSign = String(buttonTitle.dropLast())
         let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
-        
         tip = buttonTitleAsANumber / 100
         
     }
@@ -55,10 +53,19 @@ class CalculatorViewController: UIViewController {
             finalResult = String(format: "%.2f", result)
         }
         
-        print(String(format: "%.1f", tip))
-        print(numberOfPeople)
-        print(finalResult)
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+        
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "goToResult" {
+            
+            let destinationVC = segue.destination as! ResultsViewController
+            destinationVC.result = finalResult
+            destinationVC.tip = Int(tip * 100)
+            destinationVC.split = numberOfPeople
+        }
+    }
 }
 
